1fc1fd90b1789939fe6c84601b94757d5781f92c
Renan Duarte <renandg.axl@gmail.com>
2024-10-24 18:55:18 -0300

    service-registry microservice project created and configured

---
+++ src/test/java/com/davthayren/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davthayren.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
